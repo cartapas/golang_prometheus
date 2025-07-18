b90e4d7bb970b6286665a1137d96d09f71d3d1b7
Carlos Tapasco <cartapas@gmail.com>
2025-07-18 01:06:47 -0500

    feat: adding collector

--- my-app/main.go
+++ my-app/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -21,13 +22,19 @@ type metrics struct {
 	// - prometheus.GaugeVector is a bundle of Gauges with the same name but different label. Use when
 	//     when you have several devices types.
 	devices prometheus.Gauge
+	info    *prometheus.GaugeVec // Metric for metadata
 }
 
-// Declare a global var to hold all the connected devices
-var dvs []Device
+// Global vars
+var dvs []Device   // Hold all the connected devices
+var version string // App version
 
-// Use the init function to define some devices values during application starting
+// Use the init function to define some values required for demo
 func init() {
+	// Hardcoded version for demo
+	version = "2.10.5"
+
+	// Devices values during application starting
 	dvs = []Device{
 		{1, "5F-22-CC-1F-43-82", "2.1.6"},
 		{2, "EF-2B-C4-F5-D6-34", "2.1.6"},
@@ -38,14 +45,22 @@ func init() {
 // Input: prometheus.Registerer, Output: *metrics
 func NewMetrics(reg prometheus.Registerer) *metrics {
 	m := &metrics{
+		// First metric configured for amount of devices connected
 		devices: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: "myapp",             // Namespace is a unique word prefix
-			Name:      "connected_devices", // Metric name following Prometheus conventions
-			Help:      "Number of currently connected devices.",
+			Namespace: "myapp",                                  // Namespace is a unique word prefix
+			Name:      "connected_devices",                      // Metric name following Prometheus conventions
+			Help:      "Number of currently connected devices.", // Metric description
 		}),
+		// Second metric configured for version number
+		info: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "myapp",                                     // Namespace is a unique word prefix
+			Name:      "info",                                      // Metric name following Prometheus conventions
+			Help:      "Information about the My App environment.", // Metric description
+		},
+			[]string{"version"}),
 	}
-	reg.MustRegister(m.devices) // Registering with Prometheus registry
-	return m                    // Return pointer
+	reg.MustRegister(m.devices, m.info) // Registering every metric with Prometheus registry
+	return m                            // Return pointer
 }
 
 // Add a handler to send the connected devices through the endpoint
@@ -66,13 +81,15 @@ func getDevices(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	reg := prometheus.NewRegistry() // Create a non-global registry without any collectors
-	m := NewMetrics(reg)            // Create metrics using the NewMetrics function
+	reg := prometheus.NewRegistry()               // Create a non-global registry without any collectors
+	reg.MustRegister(collectors.NewGoCollector()) // Add the included collector to the registry
+	m := NewMetrics(reg)                          // Create metrics using the NewMetrics function
 
-	m.devices.Set(float64(len(dvs))) // Set de Gauge according to the connected devices
+	m.devices.Set(float64(len(dvs)))                          // Set de Gauge according to the connected devices
+	m.info.With(prometheus.Labels{"version": version}).Set(1) // If version is empty, uses 1 as default
 
 	// Custom Prometheus handler with the new registry
-	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg})
 
 	// HTTP endpoint using the custom handler
 	http.Handle("/metrics", promHandler)
